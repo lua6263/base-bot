f1458f009523d048eff1aa9db2d804f784482a85
tetartoid <hhrrzznn@gmail.com>
2022-11-05 19:49:54 +0200

    added .env

--- app.ts
+++ app.ts
@@ -1,1 +1,41 @@
-console.log(';123123')
+import {REST, Routes, Client, Snowflake} from 'discord.js';
+
+import * as dotenv from 'dotenv';
+
+const commands = [{
+    name: 'ping',
+    description: 'Replies with Pong!',
+}]
+
+const token = dotenv.parse('TOKEN');
+
+const rest = new REST({version: '10'}).setToken(token);
+const refreshingApp = async () => {
+    try {
+        console.log('Started refreshing application (/) commands.')
+        await rest.put(Routes.(), {body: commands})
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (e) {
+        console.error(e);
+    }
+};
+
+refreshingApp().then(() => {
+    console.log('refreshed')
+});
+
+const client = new Client({ intents: [Snowflake.Guilds] });
+
+client.on('ready', () => {
+    console.log(`Logged in as ${client.user?.tag}!`);
+});
+
+client.on('interactionCreate', async interaction => {
+    if (!interaction.isChatInputCommand()) return;
+
+    if (interaction.commandName === 'ping') {
+        await interaction.reply('Pong!');
+    }
+});
+
+client.login(TOKEN);
---
+++ baseData.ts
@@ -0,0 +1,4 @@
+export const CLIENT_ID: number = 1038204197881008138;
+export const TOKEN: string = 'MTAzODIwNDE5Nzg4MTAwODEzOA.GEO-tv.Q4gMN3j-aLIHwxXeR00YKzgcU_NIu5kySbQ_RQ';
+export const APPLICATION_ID: number = 1038204197881008138;
+export const PUBLIC_KEY: string = '1aeccfc4e0207fbdc2cdb3b86b2399a802f80242147a1983e8716912ad69e287';

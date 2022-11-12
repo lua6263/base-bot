404de4f3a1f72803a377cd9429b17f6ad8803b1d
tetartoid <hhrrzznn@gmail.com>
2022-11-12 22:09:08 +0200

    updated token

--- app.ts
+++ app.ts
@@ -1,15 +1,20 @@
-import {Client} from "discord.js";
+import {Client, Routes, REST} from "discord.js";
 import {config} from "dotenv";
 
-const commands = [{
-    name: 'ping',
-    description: 'Replies with Pong!',
-}]
-
 const token = config().parsed?.['TOKEN'];
-console.log(token);
-
+const clientId = config().parsed?.['CLIENT_ID'];
+console.log({token, clientId});
 
 const client = new Client({intents: ['Guilds', 'GuildMessages']});
-
 client.login(token)
+console.log(client)
+client.on('channelCreate', () => {
+    console.log(`${client.user?.id}`, 'has logged in')
+});
+client.on('channelUpdate', (oldC, newC) => {
+    console.log('UPDATED CHANEL')
+    console.log(oldC, newC)
+})
+client.on('messageCreate', (message) => {
+    console.log(message);
+})
